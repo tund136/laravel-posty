bfed67f231c291ff5bdbda69cd63312c9855191b
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-08-15 03:50:43 +0700

    Logging out

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,12 @@
     <ul class="flex items-center">
         @auth()
             <li><a href="" class="p-3">tund</a></li>
-            <li><a href="" class="p-3">Logout</a></li>
+            <li>
+                <form action="{{route('logout')}}" method="post" class="p-3 inline">
+                    @csrf
+                    <button type="submit">Logout</button>
+                </form>
+            </li>
         @endauth
         @guest
             <li><a href="{{route('login')}}" class="p-3">Login</a></li>

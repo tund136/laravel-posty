9b7d0c93483c2830529e45202c4f580729b7f8f3
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-08-15 03:36:53 +0700

    Logging in

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller {
+    public function index() {
+        return view('auth.login');
+    }
+
+    public function store(Request $request) {
+        // Validation
+        try {
+            $this->validate($request, ['email' => ['required', 'email'], 'username' => ['required'],]);
+        } catch (ValidationException $e) {
+
+        }
+
+        // Sign the user in
+        if (!auth()->attempt($request->only('email', 'password'))) {
+            return back()->with('status', 'Invalid login details.');
+        }
+
+        // Redirect
+        return redirect()->route('dashboard');
+
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
             <li><a href="" class="p-3">Logout</a></li>
         @endauth
         @guest
-            <li><a href="" class="p-3">Login</a></li>
+            <li><a href="{{route('login')}}" class="p-3">Login</a></li>
             <li><a href="{{route('register')}}" class="p-3">Register</a></li>
         @endguest
     </ul>

29660d01fa90725328574446f3d6c2ed6d674f92
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-08-23 23:56:39 +0700

    Sending email

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\PostLiked;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class DashboardController extends Controller {
     public function __construct() {

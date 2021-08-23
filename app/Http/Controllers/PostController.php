caef8aa0bb208dbd0bfe759a5c4b59b73158f93a
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-08-23 11:09:52 +0700

    Deleting our posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -28,4 +28,10 @@ class PostController extends Controller {
 
         return back();
     }
+
+    public function destroy(Post $post) {
+        $post->delete();
+
+        return back();
+    }
 }
